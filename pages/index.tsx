e6be003e35f84a63e2a144335323b783a32adae6
AleksandrKompanetc <moneymacker7@gmail.com>
2024-10-09 23:42:59 +0300

    adding component NavMobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import Nav from '@/components/Nav';
+import NavMobile from '@/components/NavMobile';
 import React from 'react';
 
 const HomePage = () => {
   return (
     <div className='overflow-hidden'>
+      <NavMobile />
       <Nav />
     </div>
   )
