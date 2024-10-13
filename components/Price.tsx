3590b0f1b0bbdc16f67d27e0be14628f9548c2ca
AleksandrKompanetc <moneymacker7@gmail.com>
2024-10-14 00:02:51 +0300

    adding component PriceCard

--- components/Price.tsx
+++ components/Price.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import PriceCard from './Helper/PriceCard'
 
 const Price = () => {
   return (
@@ -10,7 +11,7 @@ const Price = () => {
         </h1>
       </div>
       <div className='w-[80%] mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 items-center pt-[4rem] md:pt-[7rem]'>
-        
+        <PriceCard plane='Basic' price='10$' save='25%' bg='bg-red-900' />
       </div>
     </div>
   )
