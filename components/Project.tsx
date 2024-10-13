a0c2abcafef20f29c3c52eb650e7d5c7c4a16953
AleksandrKompanetc <moneymacker7@gmail.com>
2024-10-13 22:48:05 +0300

    adding components Project and ProjectCard

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ProjectCard from './Helper/ProjectCard'
 
 const Project = () => {
   return (
@@ -7,7 +8,38 @@ const Project = () => {
         <p className='heading__mini'>Recent Works</p>
         <h1 className='heading__primary'>My Best <span className='text-yellow-300'>Projects</span></h1>
       </div>
-      
+      <ProjectCard 
+        title='E-commerce Website'
+        tech1='React'
+        tech2='Next JS'
+        tech3='Tailwind'
+        tech4='TypeScript'
+        image='/images/pro.jpg' 
+      />
+      <ProjectCard 
+        title='Portfolio Website'
+        tech1='React'
+        tech2='Next JS'
+        tech3='Tailwind'
+        tech4='TypeScript'
+        image='/images/pro1.jpg' 
+      />
+      <ProjectCard 
+        title='Fullstack Food delivery Webapp'
+        tech1='React'
+        tech2='Next JS'
+        tech3='Node JS'
+        tech4='MongoDB'
+        image='/images/pro2.jpg' 
+      />
+      <ProjectCard 
+        title='Travel Website'
+        tech1='React'
+        tech2='Next JS'
+        tech3='Node JS'
+        tech4='MongoDB'
+        image='/images/pro4.jpg' 
+      />
     </div>
   )
 }
