9e7d1f811e9a82ccdc255fa81c4510a7ecf73d04
AleksandrKompanetc <moneymacker7@gmail.com>
2024-10-13 22:59:20 +0300

    adding component Price

---
+++ components/Price.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Price = () => {
+  return (
+    <div className='pt-[5rem] pb-[3rem] bg-black'>
+      <div className='text-center'>
+        <p className='heading__mini'>Popular Services</p>
+        <h1 className='heading__primary'>
+          Best <span className='text-yellow-300'>Pricing</span> For Your Project
+        </h1>
+      </div>
+      <div className='w-[80%] mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 items-center pt-[4rem] md:pt-[7rem]'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Price
