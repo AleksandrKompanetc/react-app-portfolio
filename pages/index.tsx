19da83c48697c1edd3d374052d3a3872ef593ae5
AleksandrKompanetc <moneymacker7@gmail.com>
2024-10-15 13:42:00 +0300

    adding component Blog

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import AboutMe from '@/components/AboutMe';
+import Blog from '@/components/Blog';
 import Hero from '@/components/Hero';
 import Nav from '@/components/Nav';
 import NavMobile from '@/components/NavMobile';
@@ -26,6 +27,7 @@ const HomePage = () => {
       <Project />
       <Price />
       <Reviews />
+      <Blog />
     </div>
   )
 }
