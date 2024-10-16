c852e2bc0b37c3e69f249eafdab93efde6767ac6
AleksandrKompanetc <moneymacker7@gmail.com>
2024-10-16 11:17:20 +0300

    changing component Blog

--- components/Blog.tsx
+++ components/Blog.tsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='pt-[5rem] pb-[3rem] bg-black'>
+      <div className='text-center'>
+        <p className='heading__mini'>My Blog</p>
+        <h1 className='heading__primary'>
+          My Latest <span className='text-yellow-300'>Blog</span> and news
+        </h1>
+      </div>
+      <div className='w-[80%] mx-auto pt-[3rem] md:pt-[5rem] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3
+      items-center gap-[2rem]'>
+        <div>
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
